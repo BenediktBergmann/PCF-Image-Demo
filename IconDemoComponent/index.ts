a13808ca88f7d3620c7614a3a93b99e99373dd89
Benedikt Bergmann <github@bb-webpromotion.de>
2020-04-20 23:36:24 +0200

    Initial commit

---
+++ IconDemoComponent/index.ts
@@ -0,0 +1,80 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+
+export class IconDemoComponent implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	private imgElement:HTMLImageElement;
+
+	// PowerApps component framework framework context, "Input Properties" containing the parameters, control metadata and interface functions.
+    private _context: ComponentFramework.Context<IInputs>;
+
+	/**
+	 * Empty constructor.
+	 */
+	constructor()
+	{
+
+	}
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+	{
+		this._context = context;
+
+		// get image resource (see: https://docs.microsoft.com/en-us/powerapps/developer/component-framework/reference/resources/getresource ) 	
+		this.imgElement = document.createElement("img");
+		this._context.resources.getResource("user.png", this.setImage.bind(this, false, "png"), this.showError.bind(this));
+		
+		container.appendChild(this.imgElement);
+	}
+
+
+	private setImage(shouldUpdateOutput:boolean, fileType: string, fileContent: string): void
+	{
+		let imageUrl:string = this.generateImageSrcUrl(fileType, fileContent);
+		this.imgElement.src = imageUrl;
+	}
+
+	private generateImageSrcUrl(fileType: string, fileContent: string): string
+	{
+		return  "data:image/" + fileType + ";base64, " + fileContent;
+	}
+
+	private showError(): void
+	{
+	}
+
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView(context: ComponentFramework.Context<IInputs>): void
+	{
+		// Add code to update control view
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs
+	{
+		return {};
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void
+	{
+		// Add code to cleanup control if necessary
+	}
+}
